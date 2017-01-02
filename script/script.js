e9045ffa395deace084e800bc801f68319d3cde4
herepremg@gmail.com <herepremg@gmail.com>
2017-01-03 00:32:35 +0530

    full updated
    full updated for desktop version.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-//stop iframe video
+//stop iframe video, when modal video cancel
 $(document).ready(function() {
   $('#play-vimeo-video').on('hidden.bs.modal', function() {
     var $this = $(this).find('iframe'),
@@ -7,7 +7,7 @@ $(document).ready(function() {
     $this.attr('src', tempSrc);
   });
 
-//stop html5 video
+//stop html5 video, when modal video cancel
   $('#html5Video').on('hidden.bs.modal', function() {
     var html5Video = document.getElementById("htmlVideo");
     if (html5Video != null) {
@@ -15,4 +15,21 @@ $(document).ready(function() {
       html5Video.currentTime = 0;
     }
   });
+});
+
+
+//Thumbnail Equal Height
+function equalHeight(group) {
+    tallest = 0;
+    group.each(function() {
+        thisHeight = $(this).height();
+        if(thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.each(function() { $(this).height(tallest); });
+}
+
+$(document).ready(function() {
+    equalHeight($(".thumbnail"));
 });
